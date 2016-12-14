e8d94eaa6b7050f0a234ded2d9e5f29f853cb776
erick <erickmarcos1014@gmail.com>
2016-12-14 19:48:50 +0800

    Post works perfectly

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,51 @@
 class PostsController < ApplicationController
+before_action :find_params, only:[:show, :edit, :update, :destroy]
 
 	def index
-		@posts = Post.all
+		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		
 	end
 
 	def new
-		@posts = Post.new
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
 		if @post.save
-			redirect_to post_path(@post)
+			redirect_to root_path
 		else
 			render 'new'
 		end
 	end
 
+	def edit
+
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to post_path
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
 	private
+
 		def post_params
 			params.require(:post).permit(:title, :body)
 		end
 
+		def find_params
+			@post = Post.find(params[:id])
+		end
 end
